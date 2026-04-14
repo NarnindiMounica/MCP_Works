dd5f81e6beaf8fd9a6fd736213a2cf58f35fc404
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-04-14 18:10:19 +0530

    weather tool

--- Weather_MCP/main.py
+++ Weather_MCP/main.py
@@ -1,11 +1,11 @@
 from mcp.server.fastmcp import FastMCP
 from tools.weather import get_weather
-from typing import Dict, Any
+
 
 mcp = FastMCP("Weather Checker")
 
 @mcp.tool()
-async def check_weather(location:str)->Dict[str, Any]:
+async def check_weather(location:str)->str:
     """get weather information for a specified location"""
     return get_weather(location)
 
